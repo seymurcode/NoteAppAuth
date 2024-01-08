1d9b8479706f79c5f6632a5fef4c2fb723c828f6
Sefa Aycicek <saycicek@ipucubilisim.com.tr>
2024-01-08 19:02:19 +0300

    bottom dialog height

--- app/src/main/java/com/sirketismi/noteapp/feature/newnote/AddNewNoteFragment.kt
+++ app/src/main/java/com/sirketismi/noteapp/feature/newnote/AddNewNoteFragment.kt
@@ -1,11 +1,15 @@
 package com.sirketismi.noteapp.feature.newnote
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.sirketismi.noteapp.databinding.FragmentAddNoteBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,6 +29,18 @@ class AddNewNoteFragment : BottomSheetDialogFragment() {
                 findNavController().navigateUp()
             }
         }
+
+        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
         return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        if (dialog is BottomSheetDialog) {
+            dialog.behavior.skipCollapsed = true
+            dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+        return dialog
     }
 }
